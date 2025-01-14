75dd2f19cd98f87f9e9c47c03a403bc8d369457b
Ian Zurutuza <ian.zurutuza@gmail.com>
2025-01-14 17:44:20 -0600

    init

--- usbpoll_oc.c
+++ usbpoll_oc.c
@@ -3,8 +3,9 @@
 #include <linux/kernel.h>
 #include <linux/usb.h>
 
-#define GCADAPTER_VID 0x057e
-#define GCADAPTER_PID 0x0337
+// PixArt dell mouse
+#define DEVICE_VID 0x04ca
+#define DEVICE_PID 0x0061
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Hannes Mann");
@@ -32,7 +33,7 @@ static unsigned short patch_endpoints(unsigned short interval) {
 						old_interval = altsettingptr->endpoint[endpoint].desc.bInterval;
 						altsettingptr->endpoint[endpoint].desc.bInterval = interval;
 
-						printk(KERN_INFO "gcadapter_oc: bInterval value of endpoint 0x%.2x set to %u.\n", altsettingptr->endpoint[endpoint].desc.bEndpointAddress, interval);
+						printk(KERN_INFO "usbpoll_oc: bInterval value of endpoint 0x%.2x set to %u.\n", altsettingptr->endpoint[endpoint].desc.bEndpointAddress, interval);
 					}
 				}
 			}
@@ -44,11 +45,11 @@ static unsigned short patch_endpoints(unsigned short interval) {
 			 */
 			int ret = usb_lock_device_for_reset(adapter_device, NULL);
 			if(ret) {
-				printk(KERN_ERR "gcadapter_oc: Warning! Failed to acquire lock for USB device (error: %d). Resetting device anyway...\n", ret);
+				printk(KERN_ERR "usbpoll_oc: Warning! Failed to acquire lock for USB device (error: %d). Resetting device anyway...\n", ret);
 			}
 			/* TODO: It might be possible to make the new bInterval value take effect without calling usb_reset_device? */
 			if(usb_reset_device(adapter_device)) {
-				printk(KERN_ERR "gcadapter_oc: Could not reset device (error: %d). bInterval value was NOT changed.\n", ret);
+				printk(KERN_ERR "usbpoll_oc: Could not reset device (error: %d). bInterval value was NOT changed.\n", ret);
 			}
 			/* Only unlock the device if usb_lock_device_for_reset succeeded. */
 			if(!ret) {
@@ -65,9 +66,9 @@ static int on_usb_notify(struct notifier_block* self, unsigned long action, void
 
 	switch(action) {
 		case USB_DEVICE_ADD:
-			if(device->descriptor.idVendor == GCADAPTER_VID && device->descriptor.idProduct == GCADAPTER_PID && adapter_device == NULL) {
+			if(device->descriptor.idVendor == DEVICE_VID && device->descriptor.idProduct == DEVICE_PID && adapter_device == NULL) {
 				adapter_device = device;
-				printk(KERN_INFO "gcadapter_oc: Adapter connected\n");
+				printk(KERN_INFO "usbpoll_oc: Adapter connected\n");
 
 				restore_interval = patch_endpoints(configured_interval);
 			}
@@ -76,7 +77,7 @@ static int on_usb_notify(struct notifier_block* self, unsigned long action, void
 		case USB_DEVICE_REMOVE:
 			if(adapter_device == device) {
 				adapter_device = NULL;
-				printk(KERN_INFO "gcadapter_oc: Adapter disconnected\n");
+				printk(KERN_INFO "usbpoll_oc: Adapter disconnected\n");
 			}
 			break;
 	}
@@ -87,9 +88,9 @@ static int on_usb_notify(struct notifier_block* self, unsigned long action, void
 static struct notifier_block usb_nb = { .notifier_call = on_usb_notify };
 
 static int usb_device_cb(struct usb_device* device, void* data) {
-	if(device->descriptor.idVendor == GCADAPTER_VID && device->descriptor.idProduct == GCADAPTER_PID && adapter_device == NULL) {
+	if(device->descriptor.idVendor == DEVICE_VID && device->descriptor.idProduct == DEVICE_PID && adapter_device == NULL) {
 		adapter_device = device;
-		printk(KERN_INFO "gcadapter_oc: Adapter connected\n");
+		printk(KERN_INFO "usbpoll_oc: Adapter connected\n");
 
 		restore_interval = patch_endpoints(configured_interval);
 	}
@@ -99,12 +100,12 @@ static int usb_device_cb(struct usb_device* device, void* data) {
 
 static int __init on_module_init(void) {
 	if(configured_interval > 255) {
-		printk(KERN_WARNING "gcadapter_oc: Invalid interval parameter specified.\n");
+		printk(KERN_WARNING "usbpoll_oc: Invalid interval parameter specified.\n");
 		configured_interval = 255;
 	}
 
 	if(configured_interval == 0) {
-		printk(KERN_WARNING "gcadapter_oc: Invalid interval parameter specified.\n");
+		printk(KERN_WARNING "usbpoll_oc: Invalid interval parameter specified.\n");
 		configured_interval = 1;
 	}
 
@@ -130,11 +131,11 @@ static int on_interval_changed(const char* value, const struct kernel_param* kp)
 
 	if(!ret) {
 		if(configured_interval > 255) {
-			printk(KERN_WARNING "gcadapter_oc: Invalid interval parameter specified.\n");
+			printk(KERN_WARNING "usbpoll_oc: Invalid interval parameter specified.\n");
 			configured_interval = 255;
 		}
 		else if(configured_interval == 0) {
-			printk(KERN_WARNING "gcadapter_oc: Invalid interval parameter specified.\n");
+			printk(KERN_WARNING "usbpoll_oc: Invalid interval parameter specified.\n");
 			configured_interval = 1;
 		}
 
